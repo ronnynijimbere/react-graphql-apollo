026fb5ece692e0eecac789663550acb774ddf485
Ronny <ronny.nijimbere86@gmail.com>
2020-04-01 10:20:57 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks';
+import { PokemonsContainer } from './containers/PokemonsContainer'
+
+
+function App() {
+
+  const client = new ApolloClient({
+    uri: 'https://graphql-pokemon.now.sh/'
+  })
+
+  return (
+    <ApolloProvider client={client}>
+    <main>
+      <PokemonsContainer />
+    </main>
+  </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ src/containers/PokemonsContainer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { Pokemon } from '../components/Pokemon'
+import { GET_POKEMONS } from '../graphql/get-pokemon'
+
+
+export function PokemonsContainer() {
+    const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+        variables: { first: 12 },
+    })
+
+    return (
+        <div className="container">
+            {pokemons && pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+        </div>      
+    )
+}
+
